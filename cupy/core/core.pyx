1ad18950d6cdc091aee7578078bb57e017fb1067
James Bradbury <jekbradbury@gmail.com>
2016-10-01 05:33:57 -0700

    raw kernel

--- cupy/core/core.pyx
+++ cupy/core/core.pyx
@@ -1438,6 +1438,7 @@ cpdef vector.vector[Py_ssize_t] _get_strides_for_nocopy_reshape(
 include "carray.pxi"
 include "elementwise.pxi"
 include "reduction.pxi"
+include "raw.pxi"
 
 
 # =============================================================================
